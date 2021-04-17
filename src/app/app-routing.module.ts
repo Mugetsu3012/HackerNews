5a9a61cfe7b55a18316d4e39d3b71d4bc4a0befe
Ramón Faúndez <faundezramon92@gmail.com>
2021-04-17 11:00:10 -0400

    [FIX] ruta top pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,18 @@ import { StoryComponent } from './story/story.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo:'story', pathMatch: 'full'
-  },{
-  path: 'story',
-  component: StoryComponent
-},{
-  path:'**',
-  redirectTo: '404'
-},
-{
-  path: '404',
-  component: Error404Component
-},
+    redirectTo: 'top', pathMatch: 'full'
+  }, {
+    path: 'top',
+    component: StoryComponent
+  }, {
+    path: '**',
+    redirectTo: '404'
+  },
+  {
+    path: '404',
+    component: Error404Component
+  },
 ];
 
 @NgModule({
